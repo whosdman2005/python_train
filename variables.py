c3018fbd3bf63b45bf010988137d6c259469c6ba
hobs <whosdman2005@gmail.com>
2017-07-30 22:33:45 -0700

    updating technique

--- variables.py
+++ variables.py
@@ -6,3 +6,6 @@ print(dir(name))
 
 # while this will provide more information about every available method
 print (help(str))
+
+# while this will show the specific method which is what "lower" method means
+print (help(str.lower))
