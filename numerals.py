e47ffa557f6d187c6132e487467cc25023be5646
hobs <whosdman2005@gmail.com>
2017-07-30 23:03:47 -0700

    adding more comments

--- numerals.py
+++ numerals.py
@@ -1,3 +1,13 @@
+# Arithmetic Operators:
+# Addition                  3 + 2
+# Subtraction             3 - 2
+# Multiplication         3 * 2
+# Division                  3 / 2
+# Floor Division        3 // 2
+# Exponent                 3 ** 2
+# Modulus                  3 % 2
+
+
 num = 3
 ber = 3.14
 
