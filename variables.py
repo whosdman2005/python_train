92d8fe73bfcc0b6c2a4884a0a3b49ab365be1b3c
hobs <whosdman2005@gmail.com>
2017-07-30 22:30:47 -0700

    techniques for string variables

---
+++ variables.py
@@ -0,0 +1,8 @@
+greeting = 'hello'
+name = ' kmusta'
+
+# this technique will identify what method you can use
+print(dir(name))
+
+# while this will provide more information about every available method
+print (help(str))
