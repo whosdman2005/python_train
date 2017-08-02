8e03b8191ddf8152eae3282566cafe725e0abc73
hobs <whosdman2005@gmail.com>
2017-08-01 22:37:16 -0700

    conversion technique

---
+++ stringTointeger.py
@@ -0,0 +1,13 @@
+# string to integer
+
+num_1 = '100'
+num_2 = '200'
+
+print (num_1 + num_2)
+# output 100200 since it treats the variables strings it combines them together
+
+num_1 = int(num_1)
+num_2 = int(num_2)
+
+print (num_1 + num_2)
+# output 300 since we have a conversion it treats the strings as integer
