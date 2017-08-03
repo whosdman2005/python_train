6fcf3a977ddd1ca325a6fe16b1a3510a973bf839
hobs <whosdman2005@gmail.com>
2017-08-02 23:08:08 -0700

    updated lesson

--- listing.py
+++ listing.py
@@ -1,11 +1,21 @@
 # tuples, lists and sets
 
 courses = ['History', ' Physics', 'Science', 'PE', 'Recess']
+courses_2 = ['Chemistry', 'English']
+
 
 print (courses[2:])  # slicing technique
+# output ['Art', 'History', ' Physics', 'Science', 'PE', 'Recess', 'Art']
 
 courses.append('Art')  # this will append Art to the lists
 print(courses)
+#output ['History', ' Physics', 'Science', 'PE', 'Recess', 'Art']
 
 courses.insert(0, 'Art')  # this will insert Art to the lists
 print(courses)
+# output ['Art', 'History', ' Physics', 'Science', 'PE', 'Recess', 'Art']
+
+# this method will extend whatever in courses + coursers2 to the final list
+courses.extend(courses_2)
+print(courses)
+# output ['Art', 'History', ' Physics', 'Science', 'PE', 'Recess', 'Art', 'Chemistry', 'English']
