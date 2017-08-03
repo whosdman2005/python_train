bc60b86531aa9b332c85f2957a64e77e4db28464
hobs <whosdman2005@gmail.com>
2017-08-02 23:00:43 -0700

    new lesson

---
+++ listing.py
@@ -0,0 +1,11 @@
+# tuples, lists and sets
+
+courses = ['History', ' Physics', 'Science', 'PE', 'Recess']
+
+print (courses[2:])  # slicing technique
+
+courses.append('Art')  # this will append Art to the lists
+print(courses)
+
+courses.insert(0, 'Art')  # this will insert Art to the lists
+print(courses)
