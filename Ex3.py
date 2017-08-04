b62f73ec7f366c2470bd0b0c4d84ae86fc0c46f8
hobs <whosdman2005@gmail.com>
2017-08-03 22:24:47 -0700

    another lesson for the day

---
+++ Ex3.py
@@ -0,0 +1,14 @@
+# from cyberuniversity
+
+fruits_list = ["oranges", "apple", "bananas", "peaches", "mangoes"]
+vegies_list = ["kales", "cabbages", "okra"]
+
+print (fruits_list)  # output  ['oranges', 'apple', 'bananas', 'peaches', 'mangoes']
+print (fruits_list[0])      # output   oranges
+print (fruits_list[2:4])    # output   ['bananas', 'peaches']
+print (vegies_list * 3)
+# output   ['kales', 'cabbages', 'okra', 'kales', 'cabbages', 'okra', 'kales', 'cabbages', 'okra']
+print (fruits_list[1:])  # output   ['apple', 'bananas', 'peaches', 'mangoes']
+print (fruits_list[:2])  # output ['oranges', 'apple']
+print (vegies_list + fruits_list)
+# output ['kales', 'cabbages', 'okra', 'oranges', 'apple', 'bananas', 'peaches', 'mangoes']
