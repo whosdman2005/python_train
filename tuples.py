6396c31c202167e2066a5711cdce439b15ab46a9
hobs <whosdman2005@gmail.com>
2017-08-06 23:16:44 -0700

    updating tuples lesson

--- tuples.py
+++ tuples.py
@@ -1,3 +1,7 @@
+# if you need to do looping and access to the list use TUPLES
+# if you need something to modify use LIST
+
+
 # Mutable
 list_1 = ['History', 'Science', 'Religion', 'Math', 'Physics', 'Chemistry', 'Drafting']
 list_2 = list_1
@@ -22,3 +26,16 @@ tuple_2 = tuple_1
 print (tuple_1)
 print (tuple_2)
 # output
+# ('History', 'Science', 'Religion', 'Math', 'Physics', 'Chemistry', 'Drafting')
+# ('History', 'Science', 'Religion', 'Math', 'Physics', 'Chemistry', 'Drafting')
+
+tuple_1[0] = 'Art'
+
+print (tuple_1)
+print (tuple_2)
+# output since its Immutable we cannot modify/delete to the list
+#     tuple_1[0] = 'Art'
+# TypeError: 'tuple' object does not support item assignment
+
+# you can do everything just like in the list
+# EXCEPT changing/deleting  the list since its IMMUTABLE
