b770da7db6210236fa0155de3a4ff6abc3405043
hobs <whosdman2005@gmail.com>
2017-08-06 21:41:34 -0700

    updated

--- loop2.py
+++ loop2.py
@@ -1,5 +1,11 @@
 courses = ['History', 'Science', 'Math', 'Biology']
 
+course_str = ', '.join(courses)  # converting the list into Strings
+
+course_str = ' - '.join(courses)
+
+new_list = course_str.split(' - ')  # converting the String into new list
+
 for index, course in enumerate(courses):
     print (index, course)
 # output
@@ -16,3 +22,16 @@ for index, course in enumerate(courses, start=1):
 # (2, 'Science')
 # (3, 'Math')
 # (4, 'Biology')
+
+print (course_str)
+# if course_str = ', '.join(courses)
+# output History, Science, Math, Biology
+
+print (course_str)
+# if course_str = ' - '.join(courses)
+# output History - Science - Math - Biology
+
+print (course_str)
+print (new_list)
+# output
+# back into list ['History', 'Science', 'Math', 'Biology']
