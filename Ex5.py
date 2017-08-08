1ac09e568c8ce098f76e4f27e5e29d00f9757c26
hobs <whosdman2005@gmail.com>
2017-08-07 22:14:22 -0700

    dictionary lesson

---
+++ Ex5.py
@@ -0,0 +1,10 @@
+# from cyberuniversity
+
+foods = {"fruit": "apple", "vegetables": "kale", "meat": "beef", "carb": "cereal"}
+
+print (foods)
+# output
+# {'carb': 'cereal', 'vegetables': 'kale', 'fruit': 'apple', 'meat': 'beef'}
+
+print (foods["vegetables"])
+# output  kale
