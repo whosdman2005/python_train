55882dcbb330a9696b6f242f1d98d659c4cb0a7c
hobs <whosdman2005@gmail.com>
2017-08-08 20:42:27 -0700

    updated

--- empty.py
+++ empty.py
@@ -1,11 +1,11 @@
 # empty lists
 empty_list = []
-empty_list = list()
+empty_list = list()  # always use this if you want to have an empty lists
 
 # empty tuples
 empty_tuple = ()
-empty_tuple = tuple()
+empty_tuple = tuple()  # always use this if you want to have an empty tuple
 
 # empty sets
 empty_set = {}
-empty_set = set()
+empty_set = set()  # always use this if you want to have an empty set
