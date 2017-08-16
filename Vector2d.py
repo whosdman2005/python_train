c1024b7d851009f45097c099e909c65220b84ab5
hobs <whosdman2005@gmail.com>
2017-08-15 19:54:00 -0700

    inheritance lesson

---
+++ Vector2d.py
@@ -0,0 +1,5 @@
+# practiciing modular coding
+class Vector2D:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
---
+++ mod.py
@@ -0,0 +1,16 @@
+# practiciing modular coding Vector2d.py will be the base to be imported to mod.py
+import Vector2d
+
+
+def Main():
+    vec1 = Vector2d.Vector2D(5, 7)
+    vec2 = Vector2d.Vector2D(1, 2)
+
+    print vec1.x
+    print vec1.y
+    print vec2.x
+    print vec2.y
+
+
+if __name__ == '__main__':
+    Main()
