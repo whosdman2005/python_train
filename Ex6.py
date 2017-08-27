2055992e1ef638d58aa1eeb0a8f4ab533fff1aaa
hobs <whosdman2005@gmail.com>
2017-08-26 18:03:23 -0700

    new lesson

---
+++ Ex6.py
@@ -0,0 +1,9 @@
+# from cyberuniversity
+
+name = input("Whats your name: ")
+
+age = input("How old are u?  ")
+
+location = input("Where do u live?: ")
+
+print ("you are", name, "from", location, "and you are", age "years old")
