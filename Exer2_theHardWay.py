ab775fedc43f72d8c3c6dbd7558400ce4d2da40d
hob <whosdman2005@gmail.com>
2018-09-09 22:35:27 -0700

    New lessons

---
+++ Exer2_theHardWay.py
@@ -0,0 +1,10 @@
+# This exercise came from Python 3 learn the hard way series
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by python.
+
+print ("I could have code like this.") # add the comment after is ignored
+
+# you can also use a comment to "disable" or comment out a piece of code:
+# print "This wont run"
+
+print ("This will run")
---
+++ Exer3_theHardWay.py
@@ -0,0 +1,3 @@
+# This exercise came from Python learn the hard way series
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by python.
