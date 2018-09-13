2df343aa0351fdd1c7a4d60d41f78cf0c349540e
hob <whosdman2005@gmail.com>
2018-09-12 22:09:34 -0700

    Lessong for 09/12/18

--- Exer8theHardWay.py
+++ Exer8theHardWay.py
@@ -1,5 +1,15 @@
 # Printing, Printing
-
+# %r ---> raw programmers version of variable, also known as "representation"
 formatter = ("%r %r %r %r")
 
 print (formatter % (1, 2, 3, 4))
+print (formatter % ("one", "two", "three", "four"))
+print (formatter % (True, False, False, True))
+print (formatter % (formatter, formatter, formatter, formatter))
+print (formatter % (
+    "I had this thing.",
+    "That you could type up right.",
+    "But it didn't sing.",
+    "So I said goodnight."
+
+))
