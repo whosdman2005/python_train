d48ea4241822383c496392c5c0733c17f3162295
hob <whosdman2005@gmail.com>
2018-09-30 19:20:59 -0700

    Lessons for 09/30/18

--- Exer28theHardWay.py
+++ Exer28theHardWay.py
@@ -1,1 +1,42 @@
 # Boolean Practice
+
+True and True       # true
+
+False and True      # false
+
+1 == 1 and 2 == 1   # false
+
+'test' == "test"    # true
+
+1 == 1 or 2 != 1    # true
+
+True and 1 == 1   # true
+
+False and 0 != 0  # false
+
+True or 1 == 1  #  true
+
+"test" == "testing"  # false
+
+1 != 0 and 2 == 1   # false
+
+"test" != "testing"  # true
+
+"test" == 1  # false
+
+not (True and False)  #true
+
+not (1 == 1 and 0 != 1)  # false
+
+not (10 == 1 or 1000 == 1000)
+
+not (1 != 10 or 3 == 4)
+
+not ("testing" == "testing" and "Zed" == "Cool Guy")
+
+1 == 1 and not ("testing" == 1 or 1 == 0)
+
+"chunky" == "bacon" and not (3 == 4 or 3 == 3)
+
+3 == 3 and not ("testing" == "testing" or "Python" == "fun")
+
