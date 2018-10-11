309d287cb683a7cb5e0e2795093e3b064e46b2e2
hob <whosdman2005@gmail.com>
2018-10-10 21:32:40 -0700

    Lessons for 10/10/18

---
+++ Exer34theHardWay.py
@@ -0,0 +1,28 @@
+# Lists
+# Remember: ordinal == ordered, 1st; cardinal == cards at random, 0.
+
+animals = ['bear','python','peacock','kangaroo','whale','platypus']
+
+pets = animals[1]
+print("The animal at 1 is",pets)
+
+hayop = animals[2]
+print("The third animal is ",hayop)
+
+una = animals[0]
+print("The first animal is ",una)
+
+third = animals[3]
+print("The animal at 3 is ",third)
+
+fifth = animals[5]
+print("The fifth animal is ",fifth)
+
+two = animals[3]
+print("The animal at 2 is ",two)
+
+sixth = animals[5]
+print("The sixth animal is ",sixth)
+
+forth = animals[4]
+print("The animal at 4 is ",forth)
