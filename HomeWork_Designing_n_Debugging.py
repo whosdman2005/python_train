e4a963deb4501122ebf9852dee306e2ba1e1e5b0
hob <whosdman2005@gmail.com>
2018-10-11 22:17:22 -0700

    Lessons for 10/11/18

---
+++ HomeWork_Designing_n_Debugging.py
@@ -0,0 +1,19 @@
+# From https://www.souravsengupta.com/cds2015/python/LPTHW.pdf
+# page 121
+
+# Homework
+# Now write a similar game to Exer25theHardWay.py It can be any kind of game you want in the same flavour.
+# Spend a week on it, making it as interesting as possible. For Study Drills, use lists, functions, modules
+# as much as possible, and find as many new pieces of Python as you can to make the game work.
+# However, before you start coding, you must WRITE UP a MAP for your game. Create the rooms, monsters, and traps
+# that the player must go through on paper before you CODE.
+
+# Once you have your MAP, try to code it up. If you find problems with the map, then ADJUST IT and make the code MATCH.
+
+# One final word of advice: ALL PROGRAMMERS BECOME PARALYZED BY IRRATIONAL FEAR STARTING A NEW LARGE PROJECT.
+# They procrastinate to AVOID CONFRONTING THIS FEAR and end up not getting their program working or even started.
+# I DO THIS. EVERYONE DOES. The best way to AVOID THIS is to MAKE A LIST of things you should do and then DO THEM ONE AT A TIME!!!
+
+# JUST START DOING IT, DO A SMALL VERSION, MAKE IT BIGGER, KEEP A LIST OF THINGS TO DO, AND DO THEM!!!
+
+
