f43cb96132fb9a404b51aa97bd255f84c620b4fa
hob <whosdman2005@gmail.com>
2018-10-15 21:39:03 -0700

    Lessons for 10/15/18

--- HomeWork_Designing_n_Debugging.py
+++ HomeWork_Designing_n_Debugging.py
@@ -17,3 +17,143 @@
 # JUST START DOING IT, DO A SMALL VERSION, MAKE IT BIGGER, KEEP A LIST OF THINGS TO DO, AND DO THEM!!!
 
 
+# Game story:
+# Intro
+
+from sys import exit
+
+def start():
+    print("Welcome to the DnD where your magical dreams can be explored")
+    print("Pick a character?")
+    print("Warrior, Cleric, Mage, Orc, Sorcerer, Thief")
+
+    next = input(">>>> ")
+
+    if next == "Warrior":
+        Warrior_stadium()
+    elif next == "Cleric":
+        Cleric_church()
+    elif next == "Mage":
+        Merlins_temple()
+    elif next == "Orc":
+        SwampLands()
+    elif next == "Sorcerer":
+        Hogwarts()
+    elif next == "Thief":
+        RobinHoodsLand()
+    else:
+        dead("You stumble around the room until you starve.")
+
+def Warrior_training():
+    print("You say to the squire: I am not ready, I shall sharpen thy skills and lift weights")
+    print("So leave me alone and feed my horse")
+    exit(0)
+
+def dead(why):
+    print(why, "Well done! Best decision of your LIFE!!!")
+    exit(0)
+
+
+def Warrior_stadium():
+    print("You there!")
+    print("You're the next Warrior class")
+    print("Do you approach the stable or continue training?")
+    print("Choices: approach or training ")
+
+    next = input(">>>> ")
+
+    if "approach" in next:
+        Warrior_quest()
+    elif "training" in next:
+        Warrior_training()
+
+    else:
+        start()
+
+def Cleric_church():
+    print("Choices: approach or training ")
+
+    # next = input(">>>> ")
+    #
+    # if "approach" in next:
+    #     Warrior_quest()
+    # elif "training" in next:
+    #     Warrior_training("You say to the squire: I am not ready, I shall sharpen my skills and lift weights")
+    #
+    # else:
+    #     start()
+
+
+def Merlins_temple():
+    print("Choices: approach or training ")
+
+    # next = input(">>>> ")
+    #
+    # if "approach" in next:
+    #     Warrior_quest()
+    # elif "training" in next:
+    #     Warrior_training("You say to the squire: I am not ready, I shall sharpen my skills and lift weights")
+    #
+    # else:
+    #     start()
+
+
+def SwampLands():
+    print("Choices: approach or training ")
+
+    # next = input(">>>> ")
+    #
+    # if "approach" in next:
+    #     Warrior_quest()
+    # elif "training" in next:
+    #     Warrior_training("You say to the squire: I am not ready, I shall sharpen my skills and lift weights")
+    #
+    # else:
+    #     start()
+
+def Hogwarts():
+    print("Choices: approach or training ")
+
+    # next = input(">>>> ")
+    #
+    # if "approach" in next:
+    #     Warrior_quest()
+    # elif "training" in next:
+    #     Warrior_training("You say to the squire: I am not ready, I shall sharpen my skills and lift weights")
+    #
+    # else:
+    #     start()
+
+
+def RobinHoodsLand():
+    print("Choices: approach or training ")
+
+    # next = input(">>>> ")
+    #
+    # if "approach" in next:
+    #     Warrior_quest()
+    # elif "training" in next:
+    #     Warrior_training("You say to the squire: I am not ready, I shall sharpen my skills and lift weights")
+    #
+    # else:
+    #     start()
+
+
+
+
+
+def Warrior_quest():
+    print("The Warrior saddles his horse and begins a journey ")
+    print("Choices: dragon slaying, chopping ")
+
+    next = input(">>>> ")
+
+    if "approach" in next:
+        Warrior_quest()
+    elif "training" in next:
+        Warrior_training("You say to the squire: I am not ready, I shall sharpen my skills and lift weights")
+
+    else:
+        start()
+
+start()
