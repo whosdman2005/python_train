9cf658811077f9f70829d541bdf0fdc5b6badf69
hob <whosdman2005@gmail.com>
2019-02-16 23:28:32 -0800

    practing how lambda works in python

--- lambda_practice.py
+++ lambda_practice.py
@@ -7,3 +7,18 @@ def add (x, y):
 # in lambda form it will be
 lambda x, y: x + y
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# max of x, y
+def mx (x, y):
+    if x > y:
+        return x
+    else:
+        return y
+
+print(mx(8,5))
+
+# in lambda form
+mx = lambda x, y: x if x > y else y
+print (mx(8,5))
+
+
