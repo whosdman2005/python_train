0e74d0618449de1c5d9377deacb313e58f122d99
hob <whosdman2005@gmail.com>
2019-08-19 21:58:34 -0700

    Adding Mutable and Immutable examples

---
+++ Mutable_Immutable.py
@@ -0,0 +1,46 @@
+# Strings are immutable
+
+a = 'corey'
+print(a)
+print('Address of a is: {}'.format(id(a)))
+
+a = 'jon'
+print(a)
+print('Address of a is: {}'.format(id(a)))
+
+
+# OUTPUT
+# corey
+# Address of a is: 18210240  <<<<<<< Memory object are NOT the same
+# jon
+# Address of a is: 18210336  <<<<<<< Memory object are NOT the same
+
+# a[0] = 'C'
+# print(a)
+
+# corey
+# Address of a is: 58908096
+# Traceback (most recent call last):
+# jon
+# Address of a is: 58908192
+#   File "E:/GIT_DEV_python/python_train/Mutable_Immutable.py", line 18, in <module>
+#     a[0] = 'C'
+# TypeError: 'str' object does not support item assignment
+# Since string is IMMUTABLE you cant overwrite a particular field in the LIST
+
+
+# LIST data structure are MUTABLE
+b = [1,2,3,4,5,6,7]
+print(b)
+print('Address of b is: {}'.format(b))
+
+b[0] = '9'
+print(b)
+print('Address of b is: {}'.format(b))
+
+# OUTPUT
+# [1, 2, 3, 4, 5, 6, 7]
+# Address of b is: [1, 2, 3, 4, 5, 6, 7]
+
+# ['9', 2, 3, 4, 5, 6, 7]
+# Address of b is: ['9', 2, 3, 4, 5, 6, 7]
--- intro.py
+++ intro.py
@@ -1,9 +1,9 @@
-import my_module
+import sys
+sys.path.append('e:\\GIT_DEV_python\\trial')
+
+from my_module import find_index, test
 
 
 courses = ['History', 'Math', 'Physics', 'P.E', 'Recess']
-
-index = my_module.find_index(courses, 'Math')
-print(index)
-
-
+index = find_index(courses, 'Math')
+print(sys.path)
